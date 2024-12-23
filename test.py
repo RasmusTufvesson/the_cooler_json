251acfc4d6cfbe4feb0abe9d42e111530976cc01
RasmusTufvesson <lars@bizzbook.com>
2024-12-24 00:55:04 +0100

    forgot to remove some comments

--- test.py
+++ test.py
@@ -4,15 +4,3 @@ with the_cooler_json.reddit('test.json') as efg:
     d.sowhatsit()
     d.nowamredty()
 print(efg.result())
-
-
-
-# class Amazing(metaclass=JS):
-#     def __init__() -> None:
-#         print("ok")
-#     a = 2
-#     def somemethod():
-#         print(this.a)
-
-# am = Amazing()
-# am.somemethod()
